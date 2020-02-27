57bdd73fe1e704e42b941c230fad4f8426e68d73
Chuck King <cking@surveycto.com>
2020-02-27 16:28:36 -0500

    switch to using plug-in parameters for intent

--- source/script.js
+++ source/script.js
@@ -1,18 +1,28 @@
 // References to field elements
-var intentName = document.getElementById('intentName');
-var intentParams = document.getElementById('intentParams');
+var intentConfigName = document.getElementById('intentConfigName');
+var intentConfigParams = document.getElementById('intentConfigParams');
 var launchIntentBtn = document.getElementById('launchIntent');
-var prefillSMSBtn = document.getElementById('prefillSMS');
 var intentResultStatusEl = document.getElementById('intentResultStatus');
 var intentResultsValueEl = document.getElementById('intentResultsValue');
 var clearIntentResultsBtn = document.getElementById('clearIntentResults');
+var saveIntentResultsBtn = document.getElementById('saveIntentResults');
+var saveResultsContainerEl = document.getElementById('saveResultsContainer');
 
-// Define what happens when the "Launch" button is clicked
+// References to values stored in the plug-in parameters
+var intentName = getPluginParameter('intentName');
+var intentParams = getPluginParameter('intentParams');
+
+// First, show the current values form the fieldParams object
+intentConfigName.innerHTML = intentName;
+intentConfigParams.innerHTML = intentParams;
+
+
+// Define the 'launch intent' function
 launchIntentBtn.onclick = function() {
-    // Since the parameters are expected in a JSON string format, we first need to convert from JSON to an object
-    var intentParamsObj = JSON.parse(intentParams.value);
+    // Since the intent parameters are expected in a JSON string format, we first need to convert from JSON to an object
+    var intentParamsObj = JSON.parse(intentParams);
     // Then, we can launch the intent using the current values from the config fields
-    launchIntent(intentName.value, intentParamsObj, function(error, result) {
+    launchIntent(intentName, intentParamsObj, function(error, result) {
         if (error) {
             // Something went wrong while launching the intent. 
             intentResultStatusEl.value = "error";
@@ -26,18 +36,16 @@ launchIntentBtn.onclick = function() {
     });
 }
 
-// Define the SMS example configuration
-prefillSMSBtn.onclick = function() {
-    var intentParamsObj = {
-        uri_data: 'smsto: 555-555-5555',
-        sms_body: 'Some text message...'
-    };
-    intentName.value = 'android.intent.action.SENDTO';
-    intentParams.value = JSON.stringify(intentParamsObj);
-}
-
 // Define what happens when the "Clear results" button is clicked
 clearIntentResultsBtn.onclick = function() {
     intentResultStatusEl.value = null;
     intentResultsValueEl.innerHTML = null;
+    saveResultsContainerEl.innerHTML = null;
+}
+
+// Define what happens when the "Save results" button is clicked
+saveIntentResultsBtn.onclick = function() {
+    var results = String(intentResultsValueEl.innerHTML);
+    setAnswer(results);
+    saveResultsContainerEl.innerHTML = "Results have been successfully saved!";
 }
