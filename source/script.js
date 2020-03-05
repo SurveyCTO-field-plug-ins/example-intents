cbde250af8189b69539f5286b36a874c5136fad7
wallace <wallace@surveycto.com>
2020-03-05 17:20:04 -0300

    SCTO-8274: Bug fixes.

--- source/script.js
+++ source/script.js
@@ -19,19 +19,28 @@ intentConfigParams.innerHTML = intentParams;
 
 // Define the 'launch intent' function
 launchIntentBtn.onclick = function() {
-    var intentParamsObj = JSON.parse(intentParams); // Since the intent parameters are expected in a JSON string format, we first need to convert from JSON to an object
+    var intentParamsObj;
+
+    try {
+        // Since the intent parameters are expected in a JSON string format, we first need to convert from JSON to an object
+        intentParamsObj = JSON.parse(intentParams);
+    } catch(err) { // deal with potential errors while parsing the JSON.
+        intentResultStatusEl.value = "error"; // update the return status
+        intentResultsValueEl.value = err.message || err; // show the error message
+        return;
+    }
+
     // Then, we can launch the intent using the current values from the config fields
     launchIntent(intentName, intentParamsObj, function(error, result) {
         if (error) {
             // Something went wrong while launching the intent...
             intentResultStatusEl.value = "error"; // update the return status
-            intentResultsValueEl.innerHTML = error; // show the error message
-            return;
+            intentResultsValueEl.value = error; // show the error message
         } else {
             // If nothing went wrong...
             intentResultStatusEl.value = "success"; // update the return status
             var resultString = JSON.stringify(result); // Since the returned values are expected to be objects, we first need to convert them to a string
-            intentResultsValueEl.innerHTML = resultString; // show the results
+            intentResultsValueEl.value = resultString; // show the results
         }
     });
 }
@@ -39,13 +48,13 @@ launchIntentBtn.onclick = function() {
 // Define what happens when the "Clear results" button is clicked
 clearIntentResultsBtn.onclick = function() {
     intentResultStatusEl.value = null;
-    intentResultsValueEl.innerHTML = null;
+    intentResultsValueEl.value = null;
     saveResultsContainerEl.innerHTML = null;
 }
 
 // Define what happens when the "Save results" button is clicked
 saveIntentResultsBtn.onclick = function() {
-    var results = String(intentResultsValueEl.innerHTML);
+    var results = String(intentResultsValueEl.value);
     setAnswer(results);
     saveResultsContainerEl.innerHTML = "Results have been successfully saved!";
 }
